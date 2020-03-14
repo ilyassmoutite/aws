f805e58e9ce081d9e84ff57340a2a3929d93e54c
ilyassmoutite <45095556+ilyassmoutite@users.noreply.github.com>
2020-03-14 17:42:51 +0100

    Update GLOBAL.sh

--- GLOBAL.sh
+++ GLOBAL.sh
@@ -903,7 +903,7 @@ echo
 	 MAGE_VER=MAGE_VER_${MAGE_SEL_VER}
 	 echo
        #MAGE_DOMAIN=$(ifconfig eth0 | grep "inet " | awk -F" " '{print $2}')
-       MAGE_DOMAIN=""
+       MAGE_DOMAIN="$1"
        MAGE_WEB_USER="myshop"
      MAGE_WEB_ROOT_PATH="/home/${MAGE_WEB_USER}/public_html"
      echo
