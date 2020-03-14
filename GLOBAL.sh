1c86b1215cbc5d0822304f723970908a37c27236
ilyassmoutite <ilyassmoutite@hotmail.com>
2020-03-14 13:55:48 +0100

    test

--- GLOBAL.sh
+++ GLOBAL.sh
@@ -903,7 +903,7 @@ echo
      MAGE_SEL_VER="1"
 	 MAGE_VER=MAGE_VER_${MAGE_SEL_VER}
 	 echo
-       MAGE_DOMAIN="127.0.0.1"
+       MAGE_DOMAIN=$(ifconfig eth0 | grep "inet " | awk -F" " '{print $2}')
        MAGE_WEB_USER="myshop"
      MAGE_WEB_ROOT_PATH="/home/${MAGE_WEB_USER}/public_html"
      echo
