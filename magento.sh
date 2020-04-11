f3a0115f77e56be4b338900f1ff0eb6fc6ca35eb
ilyassmoutite <45095556+ilyassmoutite@users.noreply.github.com>
2020-04-11 18:35:53 +0100

    Update magento.sh

--- magento.sh
+++ magento.sh
@@ -30,5 +30,55 @@ php -f install.php -- --license_agreement_accepted yes \
 --admin_username admin --admin_password admin123
 mount -t nfs4 $1:/ /var/www/html/skin
 mount -t nfs4 $1:/ /var/www/html/media
+cd /home/centos/aws
+sed -i '/<session_save>/d' /var/www/html/app/etc/local.xml
+sed -i '/<global>/ a\
+ <session_save>db</session_save> \
+	<redis_session> \
+	<host>127.0.0.1</host> \
+	<port>6379</port> \
+	<password></password> \
+	<timeout>10</timeout> \
+	<persistent><![CDATA[db1]]></persistent> \
+	<db>1</db> \
+	<compression_threshold>2048</compression_threshold> \
+	<compression_lib>lzf</compression_lib> \
+	<log_level>1</log_level> \
+	<max_concurrency>64</max_concurrency> \
+	<break_after_frontend>5</break_after_frontend> \
+	<break_after_adminhtml>30</break_after_adminhtml> \
+	<first_lifetime>600</first_lifetime> \
+	<bot_first_lifetime>60</bot_first_lifetime> \
+	<bot_lifetime>7200</bot_lifetime> \
+	<disable_locking>0</disable_locking> \
+	<min_lifetime>86400</min_lifetime> \
+	<max_lifetime>2592000</max_lifetime> \
+    </redis_session> \
+    <cache> \
+        <backend>Cm_Cache_Backend_Redis</backend> \
+        <backend_options> \
+          <default_priority>10</default_priority> \
+          <auto_refresh_fast_cache>1</auto_refresh_fast_cache> \
+            <server>127.0.0.1</server> \
+            <port>6379</port> \
+            <persistent><![CDATA[db1]]></persistent> \
+            <database>1</database> \
+            <password></password> \
+            <force_standalone>0</force_standalone> \
+            <connect_retries>1</connect_retries> \
+            <read_timeout>10</read_timeout> \
+            <automatic_cleaning_factor>0</automatic_cleaning_factor> \
+            <compress_data>1</compress_data> \
+            <compress_tags>1</compress_tags> \
+            <compress_threshold>204800</compress_threshold> \
+            <compression_lib>lzf</compression_lib> \
+        </backend_options> \
+    </cache>' /var/www/html/app/etc/local.xml
+
+    #sed -i "s/false/true/" /var/www/html/app/etc/modules/Cm_RedisSession.xml
+#sed -i "s/127.0.0.1/$2/" /var/www/html/app/etc/local.xml
+touch SUCCESS
+echo "$1" >> SUCCESS
+echo "$2" >> SUCCESS
 
 touch SUCCESS
