867742196c2eb7715a695a724aad88c9475db695
ilyassmoutite <45095556+ilyassmoutite@users.noreply.github.com>
2020-04-11 17:24:35 +0100

    Update LUNSH.sh

--- LUNSH.sh
+++ LUNSH.sh
@@ -2,8 +2,6 @@ cd /home/centos/aws
 sh magento.sh 
 mount -t nfs4 $1:/ /var/www/html/skin
 mount -t nfs4 $1:/ /var/www/html/media
-umount --force  /var/www/html/skin
-umount --force  /var/www/html/media
 cd /var/www/
 rm -r html/*
 cp -r magento/* html
@@ -18,8 +16,7 @@ php -f install.php -- --license_agreement_accepted yes \
 --use_secure no --secure_base_url "" --use_secure_admin no \
 --admin_lastname Test --admin_firstname Webkul --admin_email "test@webkul.com" \
 --admin_username admin --admin_password admin123
-mount -t nfs4 $1:/ /var/www/html/skin
-mount -t nfs4 $1:/ /var/www/html/skin
+
 cd /home/centos/aws
 sed -i '/<session_save>/d' /var/www/html/app/etc/local.xml
 sed -i '/<global>/ a\
