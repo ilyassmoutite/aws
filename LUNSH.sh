ed7e72872a8df88410443e877be776e1486d4b4d
ilyassmoutite <45095556+ilyassmoutite@users.noreply.github.com>
2020-04-11 15:53:39 +0100

    Update LUNSH.sh

--- LUNSH.sh
+++ LUNSH.sh
@@ -2,21 +2,24 @@ cd /home/centos/aws
 sh magento.sh 
 mount -t nfs4 $1:/ /var/www/html/skin
 mount -t nfs4 $1:/ /var/www/html/media
-#cd /var/www/
-#rm -r html/*
-#cp -r magento/* html
+umount /var/www/html/skin
+umount /var/www/html/media
+cd /var/www/
+rm -r html/*
+cp -r magento/* html
 chmod -R 777 /var/www/html/
 cd /var/www/html/
 IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
-#php -f install.php -- --license_agreement_accepted yes \
-#--db_host magento2.c29eujmse07q.eu-west-1.rds.amazonaws.com --db_name magento --db_user admin --db_pass ilyass123 \
-#--url "http://$IP/" --use_rewrites yes \
-#--skip_url_validation "yes" \
-#--use_rewrites "no" \
-#--use_secure no --secure_base_url "" --use_secure_admin no \
-#--admin_lastname Test --admin_firstname Webkul --admin_email "test@webkul.com" \
-#--admin_username admin --admin_password admin123
-
+php -f install.php -- --license_agreement_accepted yes \
+--db_host magento2.c29eujmse07q.eu-west-1.rds.amazonaws.com --db_name magento --db_user admin --db_pass ilyass123 \
+--url "http://$IP/" --use_rewrites yes \
+--skip_url_validation "yes" \
+--use_rewrites "no" \
+--use_secure no --secure_base_url "" --use_secure_admin no \
+--admin_lastname Test --admin_firstname Webkul --admin_email "test@webkul.com" \
+--admin_username admin --admin_password admin123
+mount -t nfs4 $1:/ /var/www/html/skin
+mount -t nfs4 $1:/ /var/www/html/skin
 cd /home/centos/aws
 sed -i '/<session_save>/d' /var/www/html/app/etc/local.xml
 sed -i '/<global>/ a\
